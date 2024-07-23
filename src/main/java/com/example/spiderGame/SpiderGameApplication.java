16771945e6293cb0af230bb20dbe353d003d7a54
Pacomaroto <stivanstalinov@gmail.com>
2024-07-23 10:43:05 +0300

    add generated project

---
+++ src/main/java/com/example/spiderGame/SpiderGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.spiderGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spiderGame/SpiderGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spiderGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiderGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
