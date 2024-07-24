5ef21104b0ac4ce65970ef83d4a7fc7583864b3e
Pacomaroto <stivanstalinov@gmail.com>
2024-07-24 15:23:44 +0300

    add tests

--- src/main/java/com/example/game/GameFactory.java
+++ src/main/java/com/example/game/GameFactory.java
@@ -5,7 +5,7 @@ import com.example.game.contract.Game;
 public class GameFactory {
 
     public Game getGame(String gameName, int stake) {
-        if (gameName.equalsIgnoreCase("spiderGame")) {
+        if (gameName != null && gameName.equalsIgnoreCase("spiderGame")) {
             return new SpiderGame(gameName, stake);
         }
         return null; //here can also be added validation
---
+++ src/test/java/com/example/game/GameFactoryTest.java
@@ -0,0 +1,62 @@
+package com.example.game;
+
+import com.example.game.contract.Game;
+import com.example.model.OneRoundResponse;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class GameFactoryTest {
+    GameFactory factory = new GameFactory();
+
+    @Test
+    public void givenValidGameName_whenGetGame_thenReturnsSpiderGame() {
+
+        Game game = factory.getGame("spiderGame", 110);
+        OneRoundResponse finishedGame = game.start();
+
+        assertNotNull(game);
+        assertInstanceOf(SpiderGame.class, game);
+        assertEquals("spiderGame", finishedGame.getGameName());
+        assertEquals(110, finishedGame.getStake());
+    }
+
+    @Test
+    public void givenInvalidGameName_whenGetGame_thenReturnsNull() {
+        
+        Game game = factory.getGame("invalidGame", 100);
+
+        assertNull(game);
+    }
+
+    @Test
+    public void givenNullGameName_whenGetGame_thenReturnsNull() {
+        
+        Game game = factory.getGame(null, 100);
+
+        assertNull(game);
+    }
+
+    @Test
+    public void givenEmptyGameName_whenGetGame_thenReturnsNull() {
+        
+        Game game = factory.getGame("", 100);
+
+        assertNull(game);
+    }
+
+    @Test
+    public void givenGameNameWithDifferentCase_whenGetGame_thenReturnsSpiderGame() {
+        
+        Game game = factory.getGame("SpiderGame", 100);
+        OneRoundResponse finishedGame = game.start();
+
+        assertNotNull(game);
+        assertInstanceOf(SpiderGame.class, game);
+        assertEquals("SpiderGame", finishedGame.getGameName());
+        assertEquals(100, finishedGame.getStake());
+    }
+}
